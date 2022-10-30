f4d2548f29488f2bfb3d12f4c143f849871a8839
Yak09 <83522525+Yak09@users.noreply.github.com>
2022-10-30 05:06:51 -0300

    Ajuste para use de CORS

--- src/index.js
+++ src/index.js
@@ -6,13 +6,18 @@ const morgan = require("morgan");
 
 
 const app = express();
+app.use(cors());
+
 const PORT=process.env.PORT || 3000;
 
 app.use("/api/v1/pokemon",v1pokemonRouter);
+app.use(cors({
+    methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']
+}));
 
 app.use(morgan('dev'));
 app.use(express.json());
-app.use(cors());
+
 
 
 app.listen(PORT,()=>{console.log("Server runing")});
